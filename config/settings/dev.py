fee006aa6da29e658503a30962aa59acb9fabaf1
Ehsan Barghamadi <ehsan0barghamadi@gmail.com>
2026-07-20 18:17:25 +0330

    fix(config): support SQLite in development and PostgreSQL in production

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -2,26 +2,6 @@ from .base import *
 from pathlib import Path
 from decouple import config, Csv
 
-SECRET_KEY = config('SECRET_KEY', 'default_secret_key')
-
-DEBUG = config('DEBUG', True, cast=bool)
-
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
-
-INSTALLED_APPS += [
-    # Third-party Apps
-    'localflavor',
-    'django_extensions',
-    # Local Apps
-    'page',
-    'core',
-    'user',
-    'account',
-    'store',
-    'product',
-    'order',
-    'cart',
-]
 
 DATABASES = {
     'default': {
@@ -29,27 +9,3 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-]
-
-MEDIA_URL = '/media/'
-
-MEDIA_ROOT = BASE_DIR / 'media'
-
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-
-AUTH_USER_MODEL = 'user.CustomUser'
-
-LOGIN_URL = 'user:login'
-LOGIN_REDIRECT_URL = 'page:home'
-LOGOUT_REDIRECT_URL = 'page:home'
-
-LANGUAGE_CODE = 'fa-ir'
-
-USE_I18N = True
