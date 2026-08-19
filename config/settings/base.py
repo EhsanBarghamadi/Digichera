d3c17cdc9566b0085f227cf8e9633e29c09dbf5c
ehsan barghamadi <ehsan0barghamadi@gmail.com>
2026-08-19 19:25:05 +0330

    fix(settings): add leading slash to STATIC_URL

--- config/settings/base.py
+++ config/settings/base.py
@@ -103,7 +103,7 @@ AUTHENTICATION_BACKENDS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
